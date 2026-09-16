ae56c69fd02df27b7bf788e946110e31412e69a3
Deokdragon <ejrdyd3899@gmail.com>
2026-09-16 10:39:51 +0900

    [fix] 오타 수정4

--- scripts/pull-pokemon.mjs
+++ scripts/pull-pokemon.mjs
@@ -60,7 +60,6 @@ collection.pulls = collection.pulls.slice(0, 10);
 // 아래 오류 발생 코드를 writeFile 코드로 교체합니다.
 await writeFile(DATA_PATH, `${JSON.stringify(collection, null, 2)}\n` ,"utf8");
 
-
 await updateReadme(collection);
 
 // Workflow의 커밋 메시지에서 사용합니다.
